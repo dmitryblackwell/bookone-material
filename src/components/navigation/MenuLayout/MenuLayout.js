315daac32f523012261791554fee164334fb642f
dmitryblackwell <dmitryblackwell@gmail.com>
2019-11-01 21:34:30 +0200

    Refactor navigation

--- src/components/navigation/MenuLayout/MenuLayout.js
+++ src/components/navigation/MenuLayout/MenuLayout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import clsx from "clsx";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import List from "@material-ui/core/List";
@@ -12,79 +12,15 @@ import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import Toolbar from "./Toolbar";
+import Toolbar from "./Toolbar/Toolbar";
+import { useStyles } from "./MenuLayout.style";
 
 import MenuBookOutlinedIcon from "@material-ui/icons/MenuBookOutlined";
 import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined";
 import HeadsetOutlinedIcon from "@material-ui/icons/HeadsetOutlined";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import CreateOutlinedIcon from "@material-ui/icons/CreateOutlined";
-import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined';
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    marginBottom: 100
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginRight: 36
-  },
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap"
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  drawerClose: {
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    overflowX: "hidden",
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9) + 1
-    }
-  },
-  toolbar: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    marginTop: theme.spacing(7)
-  }
-}));
+import AssignmentOutlinedIcon from "@material-ui/icons/AssignmentOutlined";
 
 export default function MiniDrawer(props) {
   const classes = useStyles();
@@ -98,6 +34,34 @@ export default function MiniDrawer(props) {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  const userLayoutData = [
+    { name: "my account", link: "/myaccount", icon: <AccountCircle /> },
+    { name: "books", link: "/books", icon: <MenuBookOutlinedIcon /> },
+    { name: "reviews", link: "/reviews", icon: <RateReviewOutlinedIcon /> },
+    { name: "podcasts", link: "/podcasts", icon: <HeadsetOutlinedIcon /> }
+  ];
+
+  const adminLayoutData = [
+    { name: "management", link: "/management", icon: <CreateOutlinedIcon /> },
+    { name: "orders", link: "/orders", icon: <AssignmentOutlinedIcon /> }
+  ]
+
+  const getLayoutList = (layoutData) => {
+    return layoutData.map(layout => {
+      return (
+        <ListItem button>
+          <ListItemIcon>
+            {layout.icon}
+          </ListItemIcon>
+          <ListItemText primary={layout.name} />
+        </ListItem>
+      );
+    });
+  }
+
+  const userLayoutListItems = getLayoutList(userLayoutData);
+  const adminLayoutListItems = getLayoutList(adminLayoutData);
 
   return (
     <div className={classes.root}>
@@ -140,45 +104,11 @@ export default function MiniDrawer(props) {
         </div>
         <Divider />
         <List>
-          <ListItem button>
-            <ListItemIcon>
-              <AccountCircle />
-            </ListItemIcon>
-            <ListItemText primary={"my account"} />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <MenuBookOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary={"books"} />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <RateReviewOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary={"reviews"} />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <HeadsetOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary={"podcasts"} />
-          </ListItem>
+          {userLayoutListItems}
         </List>
         <Divider />
         <List>
-          <ListItem button>
-            <ListItemIcon>
-              <CreateOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary={"management"} />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <AssignmentOutlinedIcon />
-            </ListItemIcon>
-            <ListItemText primary={"orders"} />
-          </ListItem>
+          {adminLayoutListItems}
         </List>
       </Drawer>
       <main className={classes.content}>{props.children}</main>
--- src/components/navigation/MenuLayout/Toolbar/Toolbar.js
+++ src/components/navigation/MenuLayout/Toolbar/Toolbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { fade, makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
@@ -12,72 +11,10 @@ import SearchIcon from "@material-ui/icons/Search";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
-import Login from './Login/Login';
+import Login from '../Login/Login';
 
-const useStyles = makeStyles(theme => ({
-  grow: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit"
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: 200
-    }
-  },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex"
-    }
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none"
-    }
-  },
-  IconButtonDesctop: {
-    marginRight: theme.spacing(1),
-  }
-}));
+import { useStyles } from './Toolbar.style';
+
 
 export default function PrimarySearchAppBar(props) {
   const classes = useStyles();
